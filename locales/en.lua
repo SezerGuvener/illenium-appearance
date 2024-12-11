9c42ea48e8e6a5d502b47a4d781af8895f87d0b7
SezerGuvener <75906653+SezerGuvener@users.noreply.github.com>
2024-12-12 01:17:46 +0300

    Add files via upload

---
+++ locales/en.lua
@@ -0,0 +1,367 @@
+Locales["en"] = {
+    UI = {
+        modal = {
+            save = {
+                title = "Özelleştirmeyi Kaydet",
+                description = "Değişiklikler kaydedilecek"
+            },
+            exit = {
+                title = "Özelleştirmeden Çık",
+                description = "Değişiklikler kaydedilmeyecek"
+            },
+            accept = "Evet",
+            decline = "Hayır"
+        },
+        ped = {
+            title = "Ped",
+            model = "Model"
+        },
+        headBlend = {
+            title = "Kalıtım",
+            shape = {
+                title = "Yüz",
+                firstOption = "Baba",
+                secondOption = "Anne",
+                mix = "Karışım"
+            },
+            skin = {
+                title = "Cilt",
+                firstOption = "Baba",
+                secondOption = "Anne",
+                mix = "Karışım"
+            },
+            race = {
+                title = "Irk",
+                shape = "Şekil",
+                skin = "Cilt",
+                mix = "Karışım"
+            }
+        },
+        faceFeatures = {
+            title = "Yüz Özellikleri",
+            nose = {
+                title = "Burun",
+                width = "Genişlik",
+                height = "Yükseklik",
+                size = "Boyut",
+                boneHeight = "Kemik Yüksekliği",
+                boneTwist = "Kemik Dönmesi",
+                peakHeight = "Tepe Yüksekliği"
+            },
+            eyebrows = {
+                title = "Kaşlar",
+                height = "Yükseklik",
+                depth = "Derinlik"
+            },
+            cheeks = {
+                title = "Yanaklar",
+                boneHeight = "Kemik Yüksekliği",
+                boneWidth = "Kemik Genişliği",
+                width = "Genişlik"
+            },
+            eyesAndMouth = {
+                title = "Gözler ve Ağız",
+                eyesOpening = "Göz Açıklığı",
+                lipsThickness = "Dudak Kalınlığı"
+            },
+            jaw = {
+                title = "Çene",
+                width = "Genişlik",
+                size = "Boyut"
+            },
+            chin = {
+                title = "Çene Ucu",
+                lowering = "Aşağı İndirme",
+                length = "Uzunluk",
+                size = "Boyut",
+                hole = "Delik Boyutu"
+            },
+            neck = {
+                title = "Boyun",
+                thickness = "Kalınlık"
+            }
+        },
+        headOverlays = {
+            title = "Görünüm",
+            hair = {
+                title = "Saç",
+                style = "Stil",
+                color = "Renk",
+                highlight = "Vurgu",
+                texture = "Doku",
+                fade = "Geçiş"
+            },
+            opacity = "Opaklık",
+            style = "Stil",
+            color = "Renk",
+            secondColor = "İkincil Renk",
+            blemishes = "Lekeler",
+            beard = "Sakal",
+            eyebrows = "Kaşlar",
+            ageing = "Yaşlanma",
+            makeUp = "Makyaj",
+            blush = "Allık",
+            complexion = "Ten Rengi",
+            sunDamage = "Güneş Hasarı",
+            lipstick = "Ruj",
+            moleAndFreckles = "Benler ve Çiller",
+            chestHair = "Göğüs Kılı",
+            bodyBlemishes = "Vücut Lekeleri",
+            eyeColor = "Göz Rengi"
+        },
+        components = {
+            title = "Kıyafetler",
+            drawable = "Model",
+            texture = "Renk",
+            mask = "Maske",
+            upperBody = "Eller",
+            lowerBody = "Bacak",
+            bags = "Çanta ve Paraşüt",
+            shoes = "Ayakkabı",
+            scarfAndChains = "Atkı ve Zincir",
+            shirt = "Gömlek",
+            bodyArmor = "Zırh",
+            decals = "Çıkartma",
+            jackets = "Ceket",
+            head = "Şapka"
+        },
+        props = {
+            title = "Aksesuarlar",
+            drawable = "Çizilebilir",
+            texture = "Doku",
+            hats = "Şapkalar ve Kasklar",
+            glasses = "Gözlükler",
+            ear = "Kulak",
+            watches = "Saatler",
+            bracelets = "Bileklikler"
+        },
+        tattoos = {
+            title = "Dövmeler",
+            items = {
+                ZONE_TORSO = "Gövde",
+                ZONE_HEAD = "Baş",
+                ZONE_LEFT_ARM = "Sol Kol",
+                ZONE_RIGHT_ARM = "Sağ Kol",
+                ZONE_LEFT_LEG = "Sol Bacak",
+                ZONE_RIGHT_LEG = "Sağ Bacak"
+            },
+            apply = "Uygula",
+            delete = "Kaldır",
+            deleteAll = "Tüm Dövmeleri Kaldır",
+            opacity = "Opaklık"
+        }
+    },
+    outfitManagement = {
+        title = "Kıyafet Yönetimi",
+        jobText = "Meslek için kıyafetleri yönet",
+        gangText = "Çete için kıyafetleri yönet"
+    },
+    cancelled = {
+        title = "Özelleştirme İptal Edildi",
+        description = "Özelleştirme kaydedilmedi"
+    },
+    outfits = {
+        import = {
+            title = "Kıyafet kodu gir",
+            menuTitle = "Kıyafet İçe Aktar",
+            description = "Paylaşım kodundan bir kıyafet içe aktar",
+            name = {
+                label = "Kıyafeti Adlandır",
+                placeholder = "Güzel bir kıyafet",
+                default = "İçe Aktarılan Kıyafet"
+            },
+            code = {
+                label = "Kıyafet Kodu"
+            },
+            success = {
+                title = "Kıyafet İçe Aktarıldı",
+                description = "Kıyafet menüsünü kullanarak şimdi kıyafeti değiştirebilirsiniz"
+            },
+            failure = {
+                title = "İçe Aktarma Başarısız",
+                description = "Geçersiz kıyafet kodu"
+            }
+        },
+        generate = {
+            title = "Kıyafet Kodu Oluştur",
+            description = "Paylaşım için bir kıyafet kodu oluştur",
+            failure = {
+                title = "Bir şeyler ters gitti",
+                description = "Kıyafet için kod oluşturulamadı"
+            },
+            success = {
+                title = "Kıyafet Kodu Oluşturuldu",
+                description = "İşte kıyafet kodunuz"
+            }
+        },
+        save = {
+            menuTitle = "Mevcut Kıyafeti Kaydet",
+            menuDescription = "Mevcut kıyafetinizi %s kıyafeti olarak kaydedin",
+            description = "Mevcut kıyafetinizi kaydedin",
+            title = "Kıyafetinizi Adlandırın",
+            managementTitle = "Yönetim Kıyafet Detayları",
+            name = {
+                label = "Kıyafet Adı",
+                placeholder = "Çok havalı bir kıyafet"
+            },
+            gender = {
+                label = "Cinsiyet",
+                male = "Erkek",
+                female = "Kadın"
+            },
+            rank = {
+                label = "Minimum Rütbe"
+            },
+            failure = {
+                title = "Kaydetme Başarısız",
+                description = "Bu isimle bir kıyafet zaten mevcut"
+            },
+            success = {
+                title = "Başarılı",
+                description = "Kıyafet %s kaydedildi"
+            }
+        },
+        update = {
+            title = "Kıyafeti Güncelle",
+            description = "Mevcut kıyafetinizi mevcut bir kıyafete kaydedin",
+            failure = {
+                title = "Güncelleme Başarısız",
+                description = "Bu kıyafet mevcut değil"
+            },
+            success = {
+                title = "Başarılı",
+                description = "Kıyafet %s güncellendi"
+            }
+        },
+        change = {
+            title = "Kıyafeti Değiştir",
+            description = "Şu anda kaydedilmiş %s kıyafetlerinden birini seçin",
+            pDescription = "Şu anda kaydedilmiş kıyafetlerden birini seçin",
+            failure = {
+                title = "Bir şeyler ters gitti",
+                description = "Değiştirmeye çalıştığınız kıyafetin temel görünümü yok"
+            }
+        },
+        delete = {
+            title = "Kıyafeti Sil",
+            description = "Kaydedilmiş bir %s kıyafetini sil",
+            mDescription = "Kaydedilmiş kıyafetlerden herhangi birini sil",
+            item = {
+                title = '"%s" Sil',
+                description = "Model: %s%s"
+            },
+            success = {
+                title = "Başarılı",
+                description = "Kıyafet Silindi"
+            }
+        },
+        manage = {
+            title = "👔 | %s Kıyafetlerini Yönet"
+        }
+    },
+    jobOutfits = {
+        title = "İş Kıyafetleri",
+        description = "İş kıyafetlerinizden birini seçin"
+    },
+    menu = {
+        returnTitle = "Geri Dön",
+        title = "Kıyafet Dolabı",
+        outfitsTitle = "Oyuncu Kıyafetleri",
+        clothingShopTitle = "Kıyafet Mağazası",
+        barberShopTitle = "Berber Dükkanı",
+        tattooShopTitle = "Dövme Dükkanı",
+        surgeonShopTitle = "Cerrah Dükkanı"
+    },
+    clothing = {
+        title = "Kıyafet Satın Al - $%d",
+        titleNoPrice = "Kıyafet Değiştir",
+        options = {
+            title = "👔 | Kıyafet Mağazası Seçenekleri",
+            description = "Giyilecek çok çeşitli ürünler arasından seçim yapın"
+        },
+        outfits = {
+            title = "👔 | Kıyafet Seçenekleri",
+            civilian = {
+                title = "Sivil Kıyafet",
+                description = "Kendi kıyafetlerinizi giyin"
+            }
+        }
+    },
+    commands = {
+        reloadskin = {
+            title = "Karakterinizi Yeniden Yükler",
+            failure = {
+                title = "Hata",
+                description = "Şu anda reloadskin kullanamazsınız"
+            }
+        },
+        clearstuckprops = {
+            title = "Varlığa bağlı tüm aksesuarları kaldırır",
+            failure = {
+                title = "Hata",
+                description = "Şu anda clearstuckprops kullanamazsınız"
+            }
+        },
+        pedmenu = {
+            title = "Kıyafet Menüsünü Aç / Ver",
+            failure = {
+                title = "Hata",
+                description = "Oyuncu çevrimdışı"
+            }
+        },
+        joboutfits = {
+            title = "İş Kıyafetleri Menüsünü Açar"
+        },
+        gangoutfits = {
+            title = "Çete Kıyafetleri Menüsünü Açar"
+        },
+        bossmanagedoutfits = {
+            title = "Patron Tarafından Yönetilen Kıyafet Menüsünü Aç"
+        },
+        textUI = {
+            clothing = "Giyim Mağazası - Fiyat: $%d",
+            barber = "Berber - Fiyat: $%d",
+            tattoo = "Dövme Dükkanı - Fiyat: $%d",
+            surgeon = "Plastik Cerrah - Fiyat: $%d",
+            clothingRoom = "Kıyafet Dolabı",
+            playerOutfitRoom = "Kıyafetler"
+        },
+        migrate = {
+            success = {
+                title = "Başarılı",
+                description = "Göç tamamlandı. %s görünüm taşındı",
+                descriptionSingle = "Taşınan Görünüm"
+            },
+            skip = {
+                title = "Bilgi",
+                description = "Atlanan görünüm"
+            },
+            typeError = {
+                title = "Hata",
+                description = "Geçersiz tür"
+            }
+        },
+        purchase = {
+            tattoo = {
+                success = {
+                    title = "Başarılı",
+                    description = "%s dövmesi %s$ karşılığında satın alındı"
+                },
+                failure = {
+                    title = "Dövme Uygulama Başarısız",
+                    description = "Yeterli paranız yok!"
+                }
+            },
+            store = {
+                success = {
+                    title = "Başarılı",
+                    description = "$%s miktarı %s kişisine verildi!"
+                },
+                failure = {
+                    title = "Yeterli Paran Yok!",
+                    description = "Yeterli paranız yok!"
+                }
+            }
+        }
+    }
+}
